828092e0ad869a0206c29f9d5d34d9474c4e136c
Jenny Magnusson <jenny.km.magnusson@gmail.com>
2025-01-02 21:27:24 +0100

    Removed comments for cleaner code

--- project/src/main/java/org/example/Menu.java
+++ project/src/main/java/org/example/Menu.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class Menu {
 
-    // meny för huvudmenyn
-    // Menu, Exit, Cities, Schools, Courses,Students,Exams
     public void showMainMenu() {
         System.out.println(
                 """  
